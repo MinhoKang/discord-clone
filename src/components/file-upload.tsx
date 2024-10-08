871a81a052eb86e44caa429cd1f9cef74fd0775b
Minho Kang <rkdalsgh0106@naver.com>
2024-10-08 15:01:05 +0900

    upload img

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import {X} from "lucide-react"
+import { X } from "lucide-react";
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
+import Image from "next/image";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
@@ -11,12 +12,28 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
-  
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image src={value} alt="Upload" className="rounded-full" fill />
+        <button
+          onClick={() => onChange("")}
+          type="button"
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
 
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
+        console.log(res);
         onChange(res?.[0]?.url);
       }}
       onUploadError={(error: Error) => {
