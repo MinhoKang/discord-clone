01511ec51beefc78700e1190170c645c253b1c6a
Minho <rkdalsgh0106@naver.com>
2024-10-09 20:39:11 +0900

    create invite code

---
+++ src/app/api/servers/[serverId]/invite-code/route.ts
@@ -0,0 +1,33 @@
+import { currentProfie } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+
+export const PATCH = async (
+  req: Request,
+  { params }: { params: { serverId: string } }
+) => {
+  try {
+    const profile = await currentProfie();
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    if (!params.serverId)
+      return new NextResponse("Server ID is missed", { status: 400 });
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        inviteCode: uuidv4(),
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
